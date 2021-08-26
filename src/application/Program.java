56b96308ae4314a475b81c8d75b8604ad089ffc5
ylmoura <yvesluan016@gmail.com>
2021-08-26 17:04:14 -0300

    Projeto finalizado

---
+++ src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entitites.Employee;
+import entitites.OutsourcedEmployee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		List<Employee> List = new ArrayList<>();
+		
+		System.out.println("Enter the number of employee: ");
+		int n = sc.nextInt();
+		
+		for (int i=1;i<=n;i++) {
+			System.out.println("Employee #"+ i + "Data");
+			System.out.printf("Outsorced(y/d)?");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name");
+			sc.nextLine(); //consumir a quebra de linha
+			String name = sc.nextLine();
+			System.out.println("Hours: ");
+			double hours = sc.nextDouble();
+			System.out.print("Value per Hour: ");
+			double valuePerHour = sc.nextDouble();
+			
+			if (ch == 'y') {
+				System.out.print("Additional charge: ");
+				double additionalCharge = sc.nextDouble();
+				list.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+			}
+			else {
+				list.add(new Employee(name, hours, valuePerHour));
+			}
+		}
+		
+		System.out.println();
+		System.out.println("PAYMENTS:");
+		for (Employee emp : list) {  // para cada funcionario na minha lista list vou mandar imprimir na tela 
+			System.out.println(emp.getName() + "-$ " + String.format("%.2f", emp.payment()));
+		}
+		
+		sc.close();
+
+	}
+
+}
